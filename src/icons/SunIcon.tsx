c180fd2641a0777d6fdf62dea10a6efaf605ad7f
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-13 15:51:28 +0530

    feat : added sun icon svg

---
+++ src/icons/SunIcon.tsx
@@ -0,0 +1,40 @@
+export const SunIcon = () => {
+    return (
+        <svg
+            className="w-6 h-6 text-black dark:text-white"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <circle cx="12" cy="12" r="6" stroke="currentColor" strokeWidth="1.5" />
+            <path d="M12 2V3" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+            <path d="M12 21V22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+            <path d="M22 12L21 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+            <path d="M3 12L2 12" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+            <path
+                d="M19.071 4.929L18.678 5.323"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+            />
+            <path
+                d="M5.322 18.678L4.929 19.071"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+            />
+            <path
+                d="M19.071 19.071L18.678 18.678"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+            />
+            <path
+                d="M5.322 5.322L4.929 4.929"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+            />
+        </svg>
+    );
+};
