577d158ded3ae19016d5c904a6975795d6fcd5c9
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-17 19:00:27 +0530

    feat: displaying the entire thing in a single layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import { useState } from 'react';
-import { AddContentModal } from './component/sharedComponents/addContentModal.component';
-import { Header } from './component/sharedComponents/header.component';
-import { Card } from './component/ui/card';
+import { lazy } from 'react';
+
+const Display = lazy(() => import('./component/sharedComponents/display.component'));
 
 function App() {
-    const [open, setOpen] = useState<boolean>(false);
-
-    let link = 'https://www.youtube.com/watch?v=6t6ZYsLXMWU';
-    let link2 = 'https://x.com/realDonaldTrump/status/1921174163848401313';
-    let link3 = 'https://www.facebook.com/photo/?fbid=3089079827933310&set=a.104999929674663';
-    let link4 = 'https://github.com/SMGhulam29091993/Second-Brain';
-    // let link4 = 'https://github.com/Bharat2044/100xDevs-Cohort3-WebDev-and-Devops';
-
     return (
-        <div className="dark:bg-black bg-slate-100 min-h-screen">
-            <Header setOpen={setOpen} />
-            <AddContentModal open={open} onClose={() => setOpen(false)} />
-            <div className="p-4 flex items-center justify-center flex-wrap gap-2">
-                <Card title={'Test Card Youtube'} link={link} type={'youtube'} />
-                <Card title={'Test Card Twitter/X'} link={link2} type={'twitter'} />
-                <Card title={'Test Card Facebook'} link={link3} type={'facebook'} />
-                <Card title={'Test Card Github'} link={link4} type={'github'} />
+        <>
+            <div className="dark:bg-black bg-slate-100 min-h-screen">
+                <Display />
             </div>
-        </div>
+        </>
     );
 }
 
