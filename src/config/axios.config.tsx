6fee34af4e42dfae071459b390f391e006e507fc
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-18 22:48:14 +0530

    fix: get token from local storage

--- src/config/axios.config.tsx
+++ src/config/axios.config.tsx
@@ -4,16 +4,13 @@ const apiUrl: string = import.meta.env.VITE_BASE_URL;
 console.log('API URL:', apiUrl);
 
 const token = localStorage.getItem('token');
-if (token) {
-    // axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    console.log('Token found:', token);
-}
 
 const api: AxiosInstance = axios.create({
     baseURL: apiUrl,
     headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
+        Authorization: token ? `Bearer ${token}` : '',
     },
     withCredentials: true,
 });
