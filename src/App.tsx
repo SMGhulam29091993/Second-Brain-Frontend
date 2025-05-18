c81e5ef76129581428c6fa1acf982b48183685d7
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-18 22:14:56 +0530

    feat: added routes and lazy loading to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const Display = lazy(() => import('./component/sharedComponents/display.component'));
+const LoginPage = lazy(() => import('./page/login'));
 
 function App() {
     return (
-        <>
-            <div className="dark:bg-black bg-slate-100 min-h-screen">
-                <Display />
-            </div>
-        </>
+        <div className="bg-slate-100 dark:bg-black">
+            <BrowserRouter>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/dashboard" element={<Display />} />
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
+        </div>
     );
 }
 
