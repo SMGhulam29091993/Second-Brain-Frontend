47e05669cce9a819a69808abb03de835a19c1269
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-18 23:33:55 +0530

    feat: added an api call create content in addContent Model

--- src/component/sharedComponents/addContentModal.component.tsx
+++ src/component/sharedComponents/addContentModal.component.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../../config/axios.config';
 import { CloseIcon } from '../../icons/CloseIcon';
-import { Button } from '../ui/button';
 import { SubmitIcon } from '../../icons/SubmitIcon';
+import { Button } from '../ui/button';
 
 interface AddContentModalProps {
     open: boolean;
@@ -25,6 +26,28 @@ export enum ContentSource {
 }
 
 export const AddContentModal: React.FC<AddContentModalProps> = ({ open, onClose }) => {
+    const [contentData, setContentData] = useState({});
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { id, value } = e.target;
+        setContentData((prev) => ({ ...prev, [id]: value }));
+    };
+
+    const handleSubmit = async () => {
+        console.log('Submit', contentData);
+
+        const res = await api.post('/content/add-content', contentData);
+        setIsLoading(true);
+        if (res.status === 200 || res.status === 201) {
+            console.log('Content added successfully');
+            setTimeout(() => {
+                setIsLoading(false);
+                setContentData({});
+                onClose();
+            }, 1000);
+        }
+        return res.data.data;
+    };
     return (
         <>
             {open && (
@@ -41,19 +64,29 @@ export const AddContentModal: React.FC<AddContentModalProps> = ({ open, onClose 
                             </div>
                             <div className="flex flex-col items-center justify-center border border-slate-300 rounded-md p-4">
                                 <input
+                                    id="title"
                                     type="text"
                                     placeholder="Add Title"
                                     className="outline-0 border border-slate-400 m-2 p-2 rounded-md w-full placeholder:text-black"
+                                    onChange={onChangeHandler}
                                 />
                                 <span className="flex items-center justify-between gap-4">
-                                    <select className="border border-slate-400 rounded-md p-2">
+                                    <select
+                                        id="type"
+                                        className="border border-slate-400 rounded-md p-2"
+                                        onChange={onChangeHandler}
+                                    >
                                         <option value={ContentType.NONE}>Select Type</option>
                                         <option value={ContentType.VIDEO}>Video</option>
                                         <option value={ContentType.IMAGE}>Image</option>
                                         <option value={ContentType.AUDIO}>Audio</option>
                                         <option value={ContentType.Article}>Article</option>
                                     </select>
-                                    <select className="border border-slate-400 rounded-md p-2">
+                                    <select
+                                        id="source"
+                                        className="border border-slate-400 rounded-md p-2"
+                                        onChange={onChangeHandler}
+                                    >
                                         <option value={ContentSource.NONE}>Select Source</option>
                                         <option value={ContentSource.YOUTUBE}>Youtube</option>
                                         <option value={ContentSource.TWITTER}>Twitter</option>
@@ -62,9 +95,11 @@ export const AddContentModal: React.FC<AddContentModalProps> = ({ open, onClose 
                                     </select>
                                 </span>
                                 <input
+                                    id="link"
                                     type="text"
                                     placeholder="Add Link"
                                     className="outline-0 border border-slate-400 m-2 p-2 rounded-md w-full placeholder:text-black"
+                                    onChange={onChangeHandler}
                                 />
                             </div>
                             <div className="flex justify-end mt-2 py-2">
@@ -73,7 +108,8 @@ export const AddContentModal: React.FC<AddContentModalProps> = ({ open, onClose 
                                     text="Submit"
                                     size="md"
                                     startIcon={<SubmitIcon />}
-                                    onClick={() => console.log('Add Content')}
+                                    onClick={handleSubmit}
+                                    loading={isLoading}
                                 />
                             </div>
                         </span>
