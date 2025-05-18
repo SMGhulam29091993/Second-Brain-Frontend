9e136b8f9405098c77c5fa8953af39c7c656599c
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-18 22:20:52 +0530

    fix : added style for sidebar

--- src/component/ui/sidebar.tsx
+++ src/component/ui/sidebar.tsx
@@ -1,7 +1,10 @@
 export const Sidebar = () => {
     return (
         <>
-            <div id="sidebar" className="w-1/5 p-4 border border-black dark:border-white"></div>
+            <div
+                id="sidebar"
+                className="bg-slate-200 dark:bg-slate-600 w-18 md:w-1/5 p-4 bg-slate-200-400 shadow-2xl"
+            ></div>
         </>
     );
 };
