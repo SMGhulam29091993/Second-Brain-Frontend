839178658f7aebd0cd67b1c727a6640dd8ed0225
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-23 18:02:26 +0530

    feat: added private route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { PrivateRoute } from './component/Auth/PrivateRoute';
+import { useAuthStore } from './store/authStore';
 
 const Display = lazy(() => import('./component/sharedComponents/display.component'));
 const LoginPage = lazy(() => import('./page/login'));
 
 function App() {
+    const token = useAuthStore((state) => state.token);
+    console.log('Token:', token);
+
     return (
         <div className="bg-slate-100 dark:bg-black">
             <BrowserRouter>
                 <Suspense fallback={<div>Loading...</div>}>
                     <Routes>
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/dashboard" element={<Display />} />
+                        <Route path="/" element={<LoginPage />} />
+                        {/* Protected Routes */}
+                        <Route element={<PrivateRoute token={token} redirect="/login" />}>
+                            <Route path="/dashboard" element={<Display />} />
+                        </Route>
+                        {/* Catch-All Route or Redirect */}
+                        <Route
+                            path="*"
+                            element={<Navigate to={token ? '/dashboard' : '/login'} />}
+                        />
                     </Routes>
                 </Suspense>
             </BrowserRouter>
