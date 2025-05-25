21c874d17a5ebe4b199afb97986f2e114cce954b
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-25 17:54:19 +0530

    feat: added pagination for display

--- src/component/sharedComponents/display.component.tsx
+++ src/component/sharedComponents/display.component.tsx
@@ -2,32 +2,57 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 import api from '../../config/axios.config';
 import { AppLayout } from '../ui/appLayout';
 import { Card } from '../ui/card';
+import { useEffect, useState } from 'react';
+import { useSourceStore } from '../../store/sourceStore';
+import { Button } from '../ui/button';
+import { PreviousIcon } from '../../icons/PreviousIcon';
+import { NextIcon } from '../../icons/NextIcon';
 
 const Display = () => {
+    const [source, setSource] = useState<string>(''); // Default source
+    const [pageNumber, setPageNumber] = useState<number>(1); // Default page number
+    const pageSize = 10; // Default page size
+
+    const getSourceFromStore = useSourceStore((state) => state.source);
+
+    useEffect(() => {
+        setSource(getSourceFromStore || ''); // Set source from store or default to empty string
+    }, [getSourceFromStore]);
+
     const fetchAllContent = async () => {
-        const response = await api.get('/content/get-all-content');
+        const queryParams = new URLSearchParams();
+        if (source) queryParams.append('source', source);
+        queryParams.append('pageNumber', pageNumber.toString());
+        queryParams.append('pageSize', pageSize.toString());
+        const response = await api.get(`/content/get-all-content?${queryParams.toString()}`);
         return response.data;
     };
 
     const queryClient = useQueryClient();
-
     queryClient.invalidateQueries({
         queryKey: ['allContent'],
     });
 
     const { isLoading, isError, data, error } = useQuery({
-        queryKey: ['allContent'],
+        queryKey: ['allContent', source, pageNumber, pageSize],
         queryFn: fetchAllContent,
     });
 
     if (isLoading) {
-        return <div className="flex flex-col items-center text-lg font-bold">Loading...</div>;
+        return (
+            <div className="dark:text-white text-black flex flex-col items-center text-lg font-bold">
+                Loading...
+            </div>
+        );
     }
 
     if (isError) {
         console.error('Error fetching data:', error);
         return <div>Something Went Wrong</div>;
     }
+
+    let totalCount = data?.data?.count || 0;
+    let totalPages = Math.ceil(totalCount / pageSize);
 
     const cardData =
         data?.data?.content.map((card: any) => ({
@@ -39,19 +64,44 @@ const Display = () => {
 
     return (
         <>
-            <div className="flex items-center justify-center gap-2 flex-wrap">
-                {cardData.length > 0 ? (
-                    cardData.map((card: any) => (
-                        <Card
-                            key={card.id}
-                            title={card.title}
-                            link={card.link}
-                            source={card.source}
-                        />
-                    ))
-                ) : (
-                    <div>No content available</div>
-                )}
+            <div className="flex flex-col items-center justify-center gap-3">
+                <div className="flex items-center justify-center gap-2 flex-wrap">
+                    {cardData.length > 0 ? (
+                        cardData.map((card: any) => (
+                            <Card
+                                key={card.id}
+                                title={card.title}
+                                link={card.link}
+                                source={card.source}
+                            />
+                        ))
+                    ) : (
+                        <div>No content available</div>
+                    )}
+                </div>
+
+                {/* Pagination */}
+                <div className="mt-3 flex justify-center items-center gap-3 fixed bottom-2 left-1/2 transform -translate-x-1/2">
+                    <Button
+                        size="sm"
+                        text="Prev"
+                        variants="primary"
+                        startIcon={<PreviousIcon />}
+                        onClick={() => setPageNumber((prev) => Math.max(prev - 1, 1))}
+                        disabled={pageNumber === 1 ? true : false}
+                    />
+                    <span className=' className="text-lg font-semibold dark:text-white text-black"'>
+                        Page {pageNumber} of {totalPages}
+                    </span>
+                    <Button
+                        size="sm"
+                        text="Next"
+                        variants="primary"
+                        startIcon={<NextIcon />}
+                        onClick={() => setPageNumber((prev) => Math.min(prev + 1, totalPages))}
+                        disabled={pageNumber === totalPages ? true : false}
+                    />
+                </div>
             </div>
         </>
     );
