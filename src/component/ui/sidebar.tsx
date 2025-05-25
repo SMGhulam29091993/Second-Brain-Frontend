b71dce1d0eb1e9a1d3c1ce91a96e3129184388c6
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-25 17:10:23 +0530

    fix: removed unwanted li and imports

--- src/component/ui/sidebar.tsx
+++ src/component/ui/sidebar.tsx
@@ -1,10 +1,19 @@
+import { HomeIcon } from '../../icons/HomeIcon';
+
 export const Sidebar = () => {
     return (
         <>
             <div
                 id="sidebar"
                 className="bg-slate-200 dark:bg-slate-600 w-18 md:w-1/5 p-4 bg-slate-200-400 shadow-2xl"
-            ></div>
+            >
+                <ul className="flex flex-col gap-2">
+                    <li className="flex items-center gap-2">
+                        <HomeIcon />
+                        <span className="dark:text-white font-medium">Dashboard</span>
+                    </li>
+                </ul>
+            </div>
         </>
     );
 };
