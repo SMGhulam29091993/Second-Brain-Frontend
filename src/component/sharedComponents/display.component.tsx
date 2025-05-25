0e17aef29777462782a0e81948133caac299c056
SMGhulam29091993 <smghulam.ghausfaiyaz19@gmail.com>
2025-05-25 20:30:06 +0530

    fix: updated the pagination logic

--- src/component/sharedComponents/display.component.tsx
+++ src/component/sharedComponents/display.component.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import api from '../../config/axios.config';
 import { NextIcon } from '../../icons/NextIcon';
 import { PreviousIcon } from '../../icons/PreviousIcon';
@@ -12,12 +12,17 @@ const Display = () => {
     const [source, setSource] = useState<string>(''); // Default source
     const [pageNumber, setPageNumber] = useState<number>(1); // Default page number
     const pageSize = 10; // Default page size
+    const [totalCount, setTotalCount] = useState<number>(0); // Total count of items
 
     const getSourceFromStore = useSourceStore((state) => state.source);
 
     useEffect(() => {
-        setSource(getSourceFromStore || ''); // Set source from store or default to empty string
-    }, [getSourceFromStore]);
+        const newSource = getSourceFromStore || '';
+        if (newSource !== source) {
+            setSource(newSource); // Set source from store or default to empty string
+            setPageNumber(1); // Reset to first page when source changes
+        }
+    }, [getSourceFromStore, source]);
 
     const fetchAllContent = async () => {
         const queryParams = new URLSearchParams();
@@ -25,6 +30,7 @@ const Display = () => {
         queryParams.append('pageNumber', pageNumber.toString());
         queryParams.append('pageSize', pageSize.toString());
         const response = await api.get(`/content/get-all-content?${queryParams.toString()}`);
+        setTotalCount(response.data.data.count || 0); // Set total count from response
         return response.data;
     };
 
@@ -32,6 +38,10 @@ const Display = () => {
         queryKey: ['allContent', source, pageNumber, pageSize],
         queryFn: fetchAllContent,
     });
+
+    useEffect(() => {
+        setTotalCount(data?.data?.count || 0); // Update total count when data changes
+    }, [source, pageNumber]);
 
     if (isLoading) {
         return (
@@ -46,7 +56,7 @@ const Display = () => {
         return <div>Something Went Wrong</div>;
     }
 
-    let totalCount = data?.data?.count || 0;
+    // let totalCount = data?.data?.count || 0;
     let totalPages = Math.ceil(totalCount / pageSize);
 
     const cardData =
@@ -77,27 +87,29 @@ const Display = () => {
                 </div>
 
                 {/* Pagination */}
-                <div className=" mt-3 flex justify-center items-center gap-3 fixed bottom-2 left-1/2 transform -translate-x-1/2">
-                    <Button
-                        size="sm"
-                        text="Prev"
-                        variants="primary"
-                        startIcon={<PreviousIcon />}
-                        onClick={() => setPageNumber((prev) => Math.max(prev - 1, 1))}
-                        disabled={pageNumber === 1 ? true : false}
-                    />
-                    <span className="text-lg font-semibold dark:text-slate-500 text-black">
-                        Page {pageNumber} of {totalPages}
-                    </span>
-                    <Button
-                        size="sm"
-                        text="Next"
-                        variants="primary"
-                        startIcon={<NextIcon />}
-                        onClick={() => setPageNumber((prev) => Math.min(prev + 1, totalPages))}
-                        disabled={pageNumber === totalPages ? true : false}
-                    />
-                </div>
+                {cardData.length > 0 && totalPages > 1 && (
+                    <div className=" mt-3 flex justify-center items-center gap-3 fixed bottom-2 left-1/2 transform -translate-x-1/2">
+                        <Button
+                            size="sm"
+                            text="Prev"
+                            variants="primary"
+                            startIcon={<PreviousIcon />}
+                            onClick={() => setPageNumber((prev) => Math.max(prev - 1, 1))}
+                            disabled={pageNumber === 1 ? true : false}
+                        />
+                        <span className="text-lg font-semibold dark:text-slate-500 text-black">
+                            Page {pageNumber} of {totalPages}
+                        </span>
+                        <Button
+                            size="sm"
+                            text="Next"
+                            variants="primary"
+                            startIcon={<NextIcon />}
+                            onClick={() => setPageNumber((prev) => Math.min(prev + 1, totalPages))}
+                            disabled={pageNumber === totalPages ? true : false}
+                        />
+                    </div>
+                )}
             </div>
         </>
     );
